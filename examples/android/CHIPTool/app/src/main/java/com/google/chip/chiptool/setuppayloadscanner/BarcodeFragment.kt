8e8e7cd5861f2b0f94dda847334cc31cb6b61ee1
Peter Yoon <hdyoon@marusys.com>
2024-01-10 11:28:24 +0900

    CHIPTool: Support 32bit ATV without Camera
      https://github.com/project-chip/connectedhomeip/blob/master/docs/guides/android_building.md
    
      $ export ANDROID_HOME=~/Android/Sdk
      $ export ANDROID_NDK_HOME=~/Android/Sdk/ndk/23.2.8568313
      $ export PATH=$PATH:[Studio Path]/jbr/bin:[Studio Path]/plugins/Kotlin/kotlinc/bin
    
      $ source scripts/bootstrap.sh
      $ ./scripts/build/build_examples.py --target android-arm-chip-tool build
      $ TARGET_CPU=arm ./scripts/examples/android_app_ide.sh
    
      Then open example/android/CHIPTool with Android Studio

--- examples/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/setuppayloadscanner/BarcodeFragment.kt
+++ examples/android/CHIPTool/app/src/main/java/com/google/chip/chiptool/setuppayloadscanner/BarcodeFragment.kt
@@ -18,33 +18,19 @@
 
 package com.google.chip.chiptool.setuppayloadscanner
 
-import android.Manifest
 import android.annotation.SuppressLint
-import android.content.pm.PackageManager
+
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.DisplayMetrics
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
-import com.google.chip.chiptool.R
 import com.google.chip.chiptool.SelectActionFragment
 import com.google.chip.chiptool.databinding.BarcodeFragmentBinding
 import com.google.chip.chiptool.util.FragmentUtil
-import com.google.mlkit.vision.barcode.BarcodeScanner
-import com.google.mlkit.vision.barcode.BarcodeScanning
-import com.google.mlkit.vision.barcode.common.Barcode
-import com.google.mlkit.vision.common.InputImage
-import java.util.concurrent.Executors
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
@@ -57,19 +43,8 @@ class BarcodeFragment : Fragment() {
   private val binding
     get() = _binding!!
 
-  private fun aspectRatio(width: Int, height: Int): Int {
-    val previewRatio = max(width, height).toDouble() / min(width, height)
-    if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
-      return AspectRatio.RATIO_4_3
-    }
-    return AspectRatio.RATIO_16_9
-  }
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    if (!hasCameraPermission()) {
-      requestCameraPermission()
-    }
   }
 
   override fun onCreateView(
@@ -78,7 +53,6 @@ class BarcodeFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View {
     _binding = BarcodeFragmentBinding.inflate(inflater, container, false)
-
     startCamera()
     binding.inputAddressBtn.setOnClickListener {
       FragmentUtil.getHost(this@BarcodeFragment, SelectActionFragment.Callback::class.java)
@@ -95,84 +69,11 @@ class BarcodeFragment : Fragment() {
 
   @SuppressLint("UnsafeOptInUsageError")
   private fun startCamera() {
-    val cameraProviderFuture = ProcessCameraProvider.getInstance(requireActivity())
-    cameraProviderFuture.addListener(
-      {
-        val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-        val metrics = DisplayMetrics().also { binding.cameraView.display?.getRealMetrics(it) }
-        val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
-        // Preview
-        val preview: Preview =
-          Preview.Builder()
-            .setTargetAspectRatio(screenAspectRatio)
-            .setTargetRotation(binding.cameraView.display.rotation)
-            .build()
-        preview.setSurfaceProvider(binding.cameraView.surfaceProvider)
-
-        // Setup barcode scanner
-        val imageAnalysis =
-          ImageAnalysis.Builder()
-            .setTargetAspectRatio(screenAspectRatio)
-            .setTargetRotation(binding.cameraView.display.rotation)
-            .build()
-        val cameraExecutor = Executors.newSingleThreadExecutor()
-        val barcodeScanner: BarcodeScanner = BarcodeScanning.getClient()
-        imageAnalysis.setAnalyzer(cameraExecutor) { imageProxy ->
-          processImageProxy(barcodeScanner, imageProxy)
-        }
-        // Select back camera as a default
-        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-        try {
-          // Unbind use cases before rebinding
-          cameraProvider.unbindAll()
-
-          // Bind use cases to camera
-          cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis)
-        } catch (exc: Exception) {
-          Log.e(TAG, "Use case binding failed", exc)
-        }
-      },
-      ContextCompat.getMainExecutor(requireActivity())
-    )
-
     // workaround: can not use gms to scan the code in China, added a EditText to debug
     binding.manualCodeBtn.setOnClickListener {
       val code = binding.manualCodeEditText.text.toString()
       Log.d(TAG, "Submit Code:$code")
       handleInputCode(code)
-    }
-  }
-
-  @ExperimentalGetImage
-  private fun processImageProxy(barcodeScanner: BarcodeScanner, imageProxy: ImageProxy) {
-    val inputImage =
-      InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
-
-    barcodeScanner
-      .process(inputImage)
-      .addOnSuccessListener { barcodes -> barcodes.forEach { handleScannedQrCode(it) } }
-      .addOnFailureListener { Log.e(TAG, it.message ?: it.toString()) }
-      .addOnCompleteListener {
-        // When the image is from CameraX analysis use case, must call image.close() on
-        // received
-        // images when finished using them. Otherwise, new images may not be received or the
-        // camera
-        // may stall.
-        imageProxy.close()
-      }
-  }
-
-  override fun onRequestPermissionsResult(
-    requestCode: Int,
-    permissions: Array<String>,
-    grantResults: IntArray
-  ) {
-    if (requestCode == REQUEST_CODE_CAMERA_PERMISSION) {
-      if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
-        showCameraPermissionAlert()
-      }
-    } else {
-      super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
   }
 
@@ -196,56 +97,6 @@ class BarcodeFragment : Fragment() {
     }
   }
 
-  private fun handleScannedQrCode(barcode: Barcode) {
-    Handler(Looper.getMainLooper()).post {
-      try {
-        val payload =
-          barcode.displayValue?.let { OnboardingPayloadParser().parseQrCode(it) } ?: return@post
-
-        FragmentUtil.getHost(this@BarcodeFragment, Callback::class.java)
-          ?.onCHIPDeviceInfoReceived(CHIPDeviceInfo.fromSetupPayload(payload))
-      } catch (ex: UnrecognizedQrCodeException) {
-        Log.e(TAG, "Unrecognized QR Code", ex)
-        Toast.makeText(requireContext(), "Unrecognized QR Code", Toast.LENGTH_SHORT).show()
-        return@post
-      }
-    }
-  }
-
-  private fun showCameraPermissionAlert() {
-    AlertDialog.Builder(requireContext())
-      .setTitle(R.string.camera_permission_missing_alert_title)
-      .setMessage(R.string.camera_permission_missing_alert_subtitle)
-      .setPositiveButton(R.string.camera_permission_missing_alert_try_again) { _, _ ->
-        requestCameraPermission()
-      }
-      .setCancelable(false)
-      .create()
-      .show()
-  }
-
-  private fun showCameraUnavailableAlert() {
-    AlertDialog.Builder(requireContext())
-      .setTitle(R.string.camera_unavailable_alert_title)
-      .setMessage(R.string.camera_unavailable_alert_subtitle)
-      .setPositiveButton(R.string.camera_unavailable_alert_exit) { _, _ ->
-        requireActivity().finish()
-      }
-      .setCancelable(false)
-      .create()
-      .show()
-  }
-
-  private fun hasCameraPermission(): Boolean {
-    return (PackageManager.PERMISSION_GRANTED ==
-      checkSelfPermission(requireContext(), Manifest.permission.CAMERA))
-  }
-
-  private fun requestCameraPermission() {
-    val permissions = arrayOf(Manifest.permission.CAMERA)
-    requestPermissions(permissions, REQUEST_CODE_CAMERA_PERMISSION)
-  }
-
   /** Interface for notifying the host. */
   interface Callback {
     /** Notifies host of the [CHIPDeviceInfo] from the scanned QR code. */
@@ -257,8 +108,5 @@ class BarcodeFragment : Fragment() {
     private const val REQUEST_CODE_CAMERA_PERMISSION = 100
 
     @JvmStatic fun newInstance() = BarcodeFragment()
-
-    private const val RATIO_4_3_VALUE = 4.0 / 3.0
-    private const val RATIO_16_9_VALUE = 16.0 / 9.0
   }
 }
